7af812869aeb19ba6603abc7dc46ec24398e0c29
Valeriia Buhaiova <bugajovalera@gmail.com>
2024-12-06 01:14:53 +0100

    add some new components and starts new pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import LocationPicker from './components/LocationPicker';
 import CustomCheckbox from './components/CustomCheckbox';
 import Filter from './components/Filter';
 import React, { useState } from 'react';
+import Date from './pages/Lost/Date';
 
 function App() {
   const [filters, setFilters] = useState({
@@ -17,8 +18,9 @@ function App() {
     <div className="App">
       {/* <Landing></Landing> */}
       {/* <LostFound></LostFound> */}
-      <Lost></Lost>
+      {/* <Lost></Lost> */}
       {/* <CustomCheckbox></CustomCheckbox> */}
+      <Date></Date>
     </div>
   );
 }
---
+++ src/pages/Lost/Date.js
@@ -0,0 +1,37 @@
+import BasicBreadcrumbs from "../../components/BasicBreadcrumbs";
+import Footer from "../../components/Footer";
+import Header from "../../components/Header";
+import styles from "../styles/Lost/Date.module.css";
+import DatePickerComponent from "../../components/DatePicker"
+
+
+const Date = () => {
+    return (
+        <div className={styles.date}>
+            <Header></Header>
+            <BasicBreadcrumbs></BasicBreadcrumbs>
+            <div className={styles.main}>
+                <div>
+                    <div className={styles.progress_bar}></div>
+                    <p className={styles.text}>Where did your pet get lost?</p>
+                </div>
+                <DatePickerComponent></DatePickerComponent>
+                <Button variant={"secondary"}>Continue</Button>
+            </div>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+const Button = ({ startIcon: StartIcon, children, variant }) => {
+    return (
+        <button
+            className={`${styles.button} ${variant === "secondary" ? styles.button_secondary: ""}`}
+        >
+            {StartIcon && <StartIcon className={styles.icon} />}
+            {children}
+        </button>
+    );
+};
+
+export default Date;
